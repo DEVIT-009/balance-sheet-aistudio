3ad6038fd5bcae2faebc85db4a38f0c105ce8b19
DEVIT-009 <helloimdevit@gmail.com>
2026-03-25 01:30:02 +0700

    modify to use mockapi io

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_B_NIN_API_BASE_URL;
 
 export const getHeaders = () => ({
   'Content-Type': 'application/json',
